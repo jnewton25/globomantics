a15748d2fde2810dbfdda7f381aa4160e87eddd4
Justin Newton <justinmnewton25@gmail.com>
2023-05-15 12:02:14 -0500

    Component setup

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,23 +1,29 @@
-import logo from "./logo.svg";
+import { useEffect, useState, useMemo } from "react";
 import "./main-page.css";
+import Header from "./header";
 
 function App() {
+	const [allHouses, setAllHouses] = useState([]);
+
+	useEffect(() => {
+		const fetchHouses = async () => {
+			const rsp = await fetch("/houses.json");
+			const houses = await rsp.json();
+			setAllHouses(houses);
+		};
+		fetchHouses();
+	}, []);
+
+	const featuredHouse = useMemo(() => {
+		if (allHouses.length) {
+			const randomIndex = Math.floor(Math.random() * allHouses.length);
+			return allHouses[randomIndex];
+		}
+	}, [allHouses]);
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div className="container">
+			<Header subtitle="Providing houses all over the world" />
 		</div>
 	);
 }
